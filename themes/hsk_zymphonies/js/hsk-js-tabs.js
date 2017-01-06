84bfeb1c5a9bfc7a6683602cb6618adc9da3b7d2
Thomas Moseler <tmoseler@gmx.de>
2017-01-06 20:49:36 +0100

    Fieldgroup Modul, Script für Tabs auf Turniere-Seiten

--- themes/hsk_zymphonies/js/hsk-js-tabs.js
+++ themes/hsk_zymphonies/js/hsk-js-tabs.js
@@ -2,7 +2,16 @@
   Drupal.behaviors.hsk_js_tabs = {
     attach: function (context, settings) {
 
-      //$('h1').hide();
+      $('.page-node-type-turnier #info-tabs').once('tabcontainer').each(function() {
+        $(this).before('<div id="table-tabs"></div>');
+
+        $('.page-node-type-turnier #info-tabs > .field').each(function(index, element){
+          $(this).find('.field__label').clone().removeClass('field__label').addClass('tab-' + index).appendTo('#table-tabs');
+          $(this).addClass('tab-' + index)
+        });
+      })
+
+
 
     }
   };
