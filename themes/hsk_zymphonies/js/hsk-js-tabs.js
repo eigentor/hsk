8f56aed3dd36e79b611b3ba7f1d083c9927d044c
Thomas Moseler <tmoseler@gmx.de>
2017-01-12 18:44:16 +0100

    Zeigen und verstecken der Tabs funktioniert

--- themes/hsk_zymphonies/js/hsk-js-tabs.js
+++ themes/hsk_zymphonies/js/hsk-js-tabs.js
@@ -7,6 +7,10 @@
        * in a separate tab.
        */
 
+      // function rufzeichen_tabs {
+      //
+      // }
+
       $('.page-node-type-turnier #info-tabs').once('tabcontainer').each(function() {
         // Add a container for the tab buttons
         $(this).before('<div id="table-tabs"></div>');
@@ -19,7 +23,25 @@
           $(this).addClass('tab-' + index);
         });
 
+        // add an active class to the active tab button and the active tab content
         $('.page-node-type-turnier #table-tabs .tab-button:first-child').addClass('active');
+        $('.page-node-type-turnier #info-tabs .field:first-child').addClass('info-tab-active');
+
+        // The show and hide function for the tabs and tab buttons
+        $('.page-node-type-turnier #table-tabs .tab-button').click(function(){
+          var $this = $(this);
+            // Get the index number of the tab that was clicked
+            var tab_number = $this.index() + 1;
+            $this.addClass('active');
+            $this.siblings().removeClass('active');
+            // Show the info-tab with the same number
+          $('.page-node-type-turnier #info-tabs .field:nth-of-type(' + tab_number + ')').each(function(){
+            $(this).show().addClass('info-tab-active');
+            $(this).siblings().hide().removeClass('info-tab-active');
+          });
+        });
+
+
 
 
       })
