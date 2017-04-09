f5571682cb4c714022b581b44c4cfc71abb780aa
Thomas Moseler <tmoseler@gmx.de>
2017-04-09 08:44:32 +0200

    Bildergalerien und Styling dafür

--- themes/hsk_zymphonies/js/hsk-fancybox.js
+++ themes/hsk_zymphonies/js/hsk-fancybox.js
@@ -2,6 +2,13 @@
   Drupal.behaviors.hsk_fancybox = {
     attach: function (context, settings) {
 
+      // Open Image gallery links in fancybox
+      $('.node .paragraph--type--image-gallery .field-collection-item--name-field-gallery-images a').fancybox({
+        infobar: false,
+        // buttons: false,
+        smallBtn: 'auto',
+        closeTpl : '<button data-fancybox-close class="fancybox-close-small"></button>',
+      });
     }
   };
 })(jQuery);
