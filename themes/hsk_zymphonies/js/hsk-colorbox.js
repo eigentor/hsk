c6dd4026d3cc0804c6973fdc74c12beb0ea46df1
Thomas Moseler <tmoseler@gmx.de>
2017-04-09 17:17:45 +0200

    Bildergalerien mit Colorbox statt Fancybox öffnen

---
+++ themes/hsk_zymphonies/js/hsk-colorbox.js
@@ -0,0 +1,15 @@
+(function ($) {
+  Drupal.behaviors.hsk_colorbox = {
+    attach: function (context, settings) {
+
+      $('.node .field--name-field-gallery-above-body .field-collection-item--name-field-gallery-images a').attr('rel', 'gallery-top');
+
+      $('.node .field--name-field-gallery-below-body .field-collection-item--name-field-gallery-images a').attr('rel', 'gallery-bottom');
+
+      // Open Image gallery links in fancybox
+      $('.node .paragraph--type--image-gallery .field-collection-item--name-field-gallery-images a').colorbox({
+
+      });
+    }
+  };
+})(jQuery);
--- themes/hsk_zymphonies/js/hsk-fancybox.js
+++ themes/hsk_zymphonies/js/hsk-fancybox.js
@@ -2,6 +2,8 @@
   Drupal.behaviors.hsk_fancybox = {
     attach: function (context, settings) {
 
+      $('.node .field--name-field-gallery-above-body .field-collection-item--name-field-gallery-images a').attr('data-fancybox', 'gallery-top');
+
       // Open Image gallery links in fancybox
       $('.node .paragraph--type--image-gallery .field-collection-item--name-field-gallery-images a').fancybox({
         infobar: false,
