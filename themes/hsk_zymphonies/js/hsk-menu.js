fbd2c12ab54d98c83d3e4e34498a1e84d82c24a4
Thomas Moseler <tmoseler@gmx.de>
2017-05-28 21:08:19 +0200

    Weiter an DWZ-Liste gearbeitet

--- themes/hsk_zymphonies/js/hsk-menu.js
+++ themes/hsk_zymphonies/js/hsk-menu.js
@@ -47,6 +47,20 @@
 
       });
 
-    }
+      // Hide empty Views columns in the DZW List
+    //   $('.view-dwz-list table tr th').each(function(i) {
+    //   //select all tds in this column
+    //   var tds = $(this).parents('table')
+    //     .find('tr td:nth-child(' + (i + 1) + ')');
+    //   //check if all the cells in this column are empty
+    //   if(tds.length == tds.filter(':empty').length) {
+    //     //hide header
+    //     $(this).hide();
+    //     //hide cells
+    //     tds.hide();
+    //   }
+    // });
+
+}
   };
 })(jQuery);
