5b85f9ae7aea9bfe2da535222212c4b2a98d423b
Thomas Moseler <tmoseler@gmx.de>
2018-12-28 20:40:30 +0100

    Modul für PGN Field Formatter

---
+++ modules/custom/hsk_pgn_formatter/src/Plugin/Field/FieldFormatter/PgnFormatter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\hsk_pgn_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Formatter to display PGN Code inside a textfield with the Chessbase PGN Viewer.
+ *
+ * @FieldFormatter (
+ *   id = 'hsk_pgn_display',
+ *   label = @Translation("Display PGN Code with the Chessbase PGN Viewer"),
+ *   field_types = {
+ *   'text_long'
+ *   }
+ * )
+ */
+
+class PgnFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    // TODO: Implement viewElements() method.
+    return $element;
+  }
+}
