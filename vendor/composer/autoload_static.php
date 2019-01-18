485d3dbb4cf8508131b34bee8e178c86bddcfd78
Thomas Moseler <tmoseler@gmx.de>
2019-01-18 08:47:37 +0100

    Core Update auf 8.6.7

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -49,6 +49,7 @@ class ComposerStaticInitDrupal8
         'T' => 
         array (
             'Twig\\' => 5,
+            'TYPO3\\PharStreamWrapper\\' => 24,
         ),
         'S' => 
         array (
@@ -223,6 +224,10 @@ class ComposerStaticInitDrupal8
         'Twig\\' => 
         array (
             0 => __DIR__ . '/..' . '/twig/twig/src',
+        ),
+        'TYPO3\\PharStreamWrapper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/typo3/phar-stream-wrapper/src',
         ),
         'Symfony\\Polyfill\\Php70\\' => 
         array (
