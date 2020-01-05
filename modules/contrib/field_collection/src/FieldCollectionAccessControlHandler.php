12d1ed1612572c3c5ab8901d7f9fb978b222488e
Thomas Moseler <tmoseler@gmx.de>
2020-01-05 19:42:25 +0100

    Field Collection Modul physisch gelöscht aus modules/contrib

--- modules/contrib/field_collection/src/FieldCollectionAccessControlHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\field_collection;
-
-use Drupal\Core\Entity\EntityAccessControlHandler;
-
-class FieldCollectionAccessControlHandler extends EntityAccessControlHandler {
-
-}
