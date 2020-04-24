094afc7623758fab06f1e8527f86a7bf0db490cf
Thomas Moseler <tmoseler@gmx.de>
2020-04-24 08:32:34 +0200

    Core Update auf 8.5.5

--- drush/drushrc.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$command_specific['config-export']['skip-modules'] = array('stage_file_proxy');
-$command_specific['config-import']['skip-modules'] = array('stage_file_proxy');
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitDrupal8::getLoader();
+return ComposerAutoloaderInit43d3b92caee1edee33166542cd3d580f::getLoader();
