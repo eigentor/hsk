70ed610c86ad5f57abdadfbf7d58bdaa1e565b12
Thomas Moseler <tmoseler@gmx.de>
2020-10-09 17:51:37 +0200

    HTTP Client mit Dependency Injection laden

--- web/modules/custom/hsk_migrate/src/Plugin/migrate/source/PlayerDwz.php
+++ web/modules/custom/hsk_migrate/src/Plugin/migrate/source/PlayerDwz.php
@@ -6,6 +6,7 @@ use Drupal\migrate_plus\Plugin\source\Url;
 use Drupal\migrate\Row;
 use Drupal\Core\Database\Database;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
 
@@ -26,18 +27,36 @@ class PlayerDwz extends Url {
    */
   protected $httpClient;
 
-  public function __construct(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+  /**
+   * PlayerDwz constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+   * @param \GuzzleHttp\ClientInterface $http_client
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, ClientInterface $http_client) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
     $this->httpClient = $http_client;
   }
 
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+   *
+   * @return static
+   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
     return new static(
-      $container->get('http_client'),
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $migration
+      $migration,
+      $container->get('http_client'),
     );
   }
 
@@ -45,8 +64,8 @@ class PlayerDwz extends Url {
    * {@inheritdoc}
    */
   function prepareRow(Row $row) {
-    $source = $row->getSource();
-    $source_id = $source['id'];
+//    $source = $row->getSource();
+//    $source_id = $source['id'];
 
 
     return parent::prepareRow($row);
