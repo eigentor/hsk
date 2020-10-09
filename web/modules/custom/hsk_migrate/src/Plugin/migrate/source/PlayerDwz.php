43f57d26f7e47149ec558a646dbd10429c39dc85
Thomas Moseler <tmoseler@gmx.de>
2020-10-09 17:33:58 +0200

    HTTP Client mit Dependency Injection laden

--- web/modules/custom/hsk_migrate/src/Plugin/migrate/source/PlayerDwz.php
+++ web/modules/custom/hsk_migrate/src/Plugin/migrate/source/PlayerDwz.php
@@ -5,6 +5,9 @@ namespace Drupal\hsk_migrate\Plugin\migrate\source;
 use Drupal\migrate_plus\Plugin\source\Url;
 use Drupal\migrate\Row;
 use Drupal\Core\Database\Database;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\migrate\Plugin\MigrationInterface;
 
 /**
  * Source plugin to only update existing player nodes
@@ -17,9 +20,35 @@ use Drupal\Core\Database\Database;
 class PlayerDwz extends Url {
 
   /**
+   * An http client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  public function __construct(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+    $this->httpClient = $http_client;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    return new static(
+      $container->get('http_client'),
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $migration
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   function prepareRow(Row $row) {
+    $source = $row->getSource();
+    $source_id = $source['id'];
+
+
     return parent::prepareRow($row);
   }
 
