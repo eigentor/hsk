42b00b258d6603eb4bc9c10c00b9b8105a13c0d6
Thomas Moseler <tmoseler@gmx.de>
2021-11-07 16:35:39 +0100

    News Node aus Block Context laden, UID des Autors der News aus News laden.

--- web/modules/custom/hsk_news/src/Plugin/Block/NewsAuthorBlock.php
+++ web/modules/custom/hsk_news/src/Plugin/Block/NewsAuthorBlock.php
@@ -7,6 +7,8 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\user\UserStorageInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\NodeInterface;
 
 /**
  * Provides a block that shows the picture and description
@@ -14,7 +16,11 @@ use Drupal\user\UserStorageInterface;
  *
  * @Block (
  *   id = "hsk_news_author",
- *   admin_label = @Translation("News Author")
+ *   admin_label = @Translation("News Author"),
+ *   category = @Translation("News"),
+ *   context_definitions = {
+ *     "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *   }
  * )
  */
 class NewsAuthorBlock extends BlockBase implements ContainerFactoryPluginInterface
@@ -27,14 +33,17 @@ class NewsAuthorBlock extends BlockBase implements ContainerFactoryPluginInterfa
 
   protected $userStorage;
 
+  protected $entityTypeManager;
+
   /*
    * @inheritdoc
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountInterface $account, UserStorageInterface $user_storage)
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountInterface $account, UserStorageInterface $user_storage, EntityTypeManagerInterface $entity_type_manager)
   {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->account = $account;
     $this->userStorage = $user_storage;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /*
@@ -43,21 +52,31 @@ class NewsAuthorBlock extends BlockBase implements ContainerFactoryPluginInterfa
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
   {
     return new static (
-      $container,
       $configuration,
       $plugin_id,
+      $plugin_definition,
       $container->get('current_user'),
-      $container->get('entity_type.manager')->getStorage('user')
+      $container->get('entity_type.manager')->getStorage('user'),
+      $container->get('entity_type.manager')
     );
   }
 
   public function build()
   {
-    $user_id = $this->account->id();
-    $user_object = $this->userStorage->load($user_id);
+    $data = $this->getUserData();
+    $peter = 7;
     return [
       '#markup' => 'Print author image and description',
     ];
+  }
+
+  public function getUserData()
+  {
+    $nid = $this->getContextValue('node')->id();
+    $news_entity = $this->entityTypeManager->getStorage('node')->load($nid);
+    $uid = $news_entity->getOwnerId();
+    $peter = 7;
+    return $news_entity;
   }
 
 
