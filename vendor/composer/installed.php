6cac36a2b8caa07b287d91c2ded73e6cc25cfd07
Thomas Moseler <tmoseler@gmx.de>
2022-11-27 23:18:20 +0100

    Twig Tweak installiert, fast schon gelingt die path() Funktion

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '07c4f15cd20c64aae921379a4c3f4584d7e14ffb',
+        'reference' => '958b54fcd90d7eec90eb510d90424233026bf250',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -813,7 +813,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '07c4f15cd20c64aae921379a4c3f4584d7e14ffb',
+            'reference' => '958b54fcd90d7eec90eb510d90424233026bf250',
             'dev_requirement' => false,
         ),
         'drupal/responsive_tables_filter' => array(
@@ -870,6 +870,15 @@
             'install_path' => __DIR__ . '/../../web/modules/contrib/token',
             'aliases' => array(),
             'reference' => '8.x-1.11',
+            'dev_requirement' => false,
+        ),
+        'drupal/twig_tweak' => array(
+            'pretty_version' => '3.2.0',
+            'version' => '3.2.0.0',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../web/modules/contrib/twig_tweak',
+            'aliases' => array(),
+            'reference' => '3.2.0',
             'dev_requirement' => false,
         ),
         'drupal/typed_data' => array(
