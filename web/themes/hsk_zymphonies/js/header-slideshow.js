f46943994900cc448db0d4b681fa5cd634381682
Thomas Moseler <tmoseler@gmx.de>
2022-11-30 16:50:31 +0100

    Letzte Styling-Feinheiten, ursprünglichen Slidesho Block ausblenden.

--- web/themes/hsk_zymphonies/js/header-slideshow.js
+++ web/themes/hsk_zymphonies/js/header-slideshow.js
@@ -4,17 +4,17 @@
 
 
 
-      $('.path-frontpage #block-views-block-header-slideshow-block-1 .view-content').slick({
-        infinite: true,
-        autoplay: true,
-        arrows: true,
-        autoplaySpeed: 6000,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        slidesToScroll: 1,
-        slide: 'div.views-row',
-      });
+      // $('.path-frontpage #block-views-block-header-slideshow-block-1 .view-content').slick({
+      //   infinite: true,
+      //   autoplay: true,
+      //   arrows: true,
+      //   autoplaySpeed: 6000,
+      //   speed: 700,
+      //   slidesToShow: 1,
+      //   slidesToScroll: 1,
+      //   slidesToScroll: 1,
+      //   slide: 'div.views-row',
+      // });
 
 
       // $('.path-frontpage #block-views-block-header-par-slideshow-block-1 .view-content').slick({
@@ -43,7 +43,7 @@
 
       // Make the height of the Slider adapt to the height of the current slide on mobile.
       // This way there is no etmpty space below if a slide is lower than the others.
-      $slickSlider = $('.path-frontpage #block-views-block-header-par-slideshow-block-1 .view-content');
+      $slickSlider = $('.path-frontpage #block-headerslideshow .block');
 
       var sliderAdaptiveHeightMobile = function() {
         $slickSlider.find('.slick-slide').height('0');
