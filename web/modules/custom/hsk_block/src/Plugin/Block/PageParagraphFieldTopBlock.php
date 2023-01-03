49ca5a3d566a617aa236df47b5f76e27ba19008d
Thomas Moseler <tmoseler@gmx.de>
2023-01-03 15:08:31 +0100

    Der Block findet schon mal den Node.

--- web/modules/custom/hsk_block/src/Plugin/Block/PageParagraphFieldTopBlock.php
+++ web/modules/custom/hsk_block/src/Plugin/Block/PageParagraphFieldTopBlock.php
@@ -7,16 +7,33 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\node\NodeInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Block for top paragraph field of Node Type "page".
+ *
+ * @package Drupal\hsk_block\Plugin\Block
+ *
+ * @Block (
+ *  id = "page_paragraph_top",
+ *  admin_label = @Translation("Page Top Paragraphs"),
+ *  category = @Translation("HSK")
+ * )
+ */
 
 class PageParagraphFieldTopBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   protected $entityTypeManager;
 
+  protected $routeMatch;
+
   public function __construct(array $configuration, $plugin_id, $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager)
+                              EntityTypeManagerInterface $entity_type_manager,
+                              RouteMatchInterface $route_match)
   {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
+    $this->routeMatch = $route_match;
   }
 
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
@@ -25,7 +42,8 @@ class PageParagraphFieldTopBlock extends BlockBase implements ContainerFactoryPl
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('current_route_match')
     );
   }
 
@@ -34,11 +52,15 @@ class PageParagraphFieldTopBlock extends BlockBase implements ContainerFactoryPl
     /** @var \Drupal\node\NodeInterface $node */
     $node = $this->routeMatch->getParameter('node');
 
+    $peter = 7;
+
     if (!($node instanceof NodeInterface)) {
       return [];
     }
 
-    $build = [];
+    $build = [
+      '#markup' => 'Platzhalter Text Page Top Paragraph Block'
+    ];
 
     // Build block render array here.
 
