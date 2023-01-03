52ae5ba878b8bf329f22740557d0b4f1cad75b84
Thomas Moseler <tmoseler@gmx.de>
2023-01-03 18:27:15 +0100

    Config für Platzierung Block Page Top Paragraphs, verschiedene Entity View Einstellungen, u.a. zusätzlicher View Mode für Infoblock, damit der Default View Mode leer sein kann und Paragraphs im normalen Content nicht angezeigt werden.

--- web/modules/custom/hsk_block/src/Plugin/Block/PageParagraphFieldTopBlock.php
+++ web/modules/custom/hsk_block/src/Plugin/Block/PageParagraphFieldTopBlock.php
@@ -8,6 +8,7 @@ use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\node\NodeInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Cache\Cache;
 
 /**
  * Block for top paragraph field of Node Type "page".
@@ -52,8 +53,6 @@ class PageParagraphFieldTopBlock extends BlockBase implements ContainerFactoryPl
     /** @var \Drupal\node\NodeInterface $node */
     $node = $this->routeMatch->getParameter('node');
 
-    $peter = 7;
-
     if (!($node instanceof NodeInterface)) {
       return [];
     }
@@ -61,27 +60,55 @@ class PageParagraphFieldTopBlock extends BlockBase implements ContainerFactoryPl
     // Return an empty render array if we don't have the paragraph type we want.
     $build = [];
 
+    // Get the viewBuilder to prepare our paragraphs for the render array
     $builder = $this->entityTypeManager->getViewBuilder('paragraph');
-    $view_mode = 'default';
+    $view_mode = 'infoblock';
 
-
-
+    // Get all paragraphs of type "infoblock" from the paragraphs reference field
+    // "field_gallery_above_body" from the node and render them in the block.
+    // Do not render other paragraph types.
     if(!empty ($node->field_gallery_above_body)) {
       $paragraphs = $node->field_gallery_above_body->referencedEntities();
-//      foreach($paragraphs->referencedEntities() as $key => $paragraph) {
-//        if($paragraph->getType() == 'infoblock') {
-//          $build['#paragraphs'] = $paragraph;
-//        }
-//      }
-      $paragraphs_items = $builder->viewMultiple($paragraphs, $view_mode);
+      foreach($paragraphs as $key => $paragraph) {
+        if($paragraph->getType() == 'infoblock') {
+          $paragraphs_infoblocks[$key] = $paragraph;
+        }
+      }
+
+      if(!empty($paragraphs_infoblocks)) {
+      $paragraphs_items = $builder->viewMultiple($paragraphs_infoblocks, $view_mode);
       $build['#paragraphs'] = $paragraphs_items;
-
-
+        $build['#theme'] = 'page_paragraph_top_block';
+      }
     }
-    $build['#theme'] = 'page_paragraph_top_block';
-
-    // Build block render array here.
 
     return $build;
+  }
+
+  /**
+   * Set Cachetags
+   *
+   * @return array|string[]
+   */
+  public function getCacheTags() {
+    // With this when your node change your block will rebuild.
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      // If there is node add its cachetag.
+      return Cache::mergeTags(parent::getCacheTags(), ['node:' . $node->id()]);
+    }
+    else {
+      // Return default tags instead.
+      return parent::getCacheTags();
+    }
+  }
+
+  /**
+   * Set cache context
+   *
+   * @return array|string[]
+   */
+  public function getCacheContexts() {
+    // Every new route this block will rebuild.
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route']);
   }
 }
